407aa04f7876ba80d6cf07e67234dfca9b4851ca
RaviTeja <104969264+crt12497@users.noreply.github.com>
2024-04-13 11:58:37 +0530

    Update credential.php

--- credential.php
+++ credential.php
@@ -1,6 +1,6 @@
 <?php 
 	/*Enter your API_KEY*/
-	define("API_KEY", '');
+	define("API_KEY", 'NDE2MTQzNzA1YTQxMzE0YzQ0N2E3NzM4NmQ1NjMzNzI=');
 
 	/*You can enter mobile number here*/
 	define("MOBILE", '');
